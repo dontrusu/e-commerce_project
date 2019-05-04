e065c5b7f1674640dbe9200b5fd726f8ab6bcb2c
dontrusu <denishandball1997@gmail.com>
2019-05-04 19:33:42 +0300

    project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 
 import Header from './components/Header/Header'
 import Search from './components/Search/Search.js';
+import Catalog from './components/Catalog/Catalog.js';
 
 
 
