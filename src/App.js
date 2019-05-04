f9ec5a141b0bcfc9e254b83c7e16bd22f2950c89
Vladislav <ignatovlad95@gmail.com>
2019-05-04 15:17:14 +0300

    Add new component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import './App.css'
 
 import Header from './components/Header/Header'
-import Search from './components/Search/Search.js';
-
+import Search from './components/Search/Search.js'
+import Social_Network from './components/Social_Network/Social_Network.js'
 
 
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
         <div className='page_wrapper'>
           <Header />
           <Search />
+          <Social_Network />
         </div>
       )
   } 
---
+++ src/components/Social_Network/Social_Network.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import './Social_Network.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faInstagram,
+  faFacebook
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faInstagram,
+  faFacebook,
+  faEnvelope
+);
+
+
+class Sicial_Network extends Component {
+    render() {
+        return (
+            <div className='social_wrapper'>
+                <a href='#'>
+                    <FontAwesomeIcon
+                        className='social_icon instagram' 
+                        icon={['fab', 'instagram']} 
+                        size="2x" 
+                        fixedWidth 
+                    />
+                </a>
+                <a href='#'>
+                    <FontAwesomeIcon 
+                        className='social_icon facebook' 
+                        icon={['fab', 'facebook']} 
+                        size="2x" 
+                        fixedWidth 
+                    />
+                </a>
+                <a href='#'>
+                    <FontAwesomeIcon
+                        className='social_icon mail'
+                        icon='envelope'
+                        size="2x"
+                        fixedWidth 
+                    />
+                </a>
+                
+                
+            </div>
+        )
+    } 
+}
+
+export default Sicial_Network;
